3ead8d593d7b3d4aed8f47324ef201d3e3b9fc1c
Salah Ansari <salah.env@gmail.com>
2025-01-15 23:22:43 +0530

    added: refinement

--- analyticsSdk/index.js
+++ analyticsSdk/index.js
@@ -6,59 +6,70 @@ const sendEvent = (value, count) => {
         }, 1000);
     });
 }
+
+function Analytics () {
+
+    let events = [];
+    let count = 1;
+
+    this.logEvent = function (event) {
+        events.push(event);
+    }
+
+    this.send = async function () {
+        const event = events.shift();
+        if(!event) {
+            console.log('all sent');
+            return;
+        }
+        try {
+            const res = await sendEvent(event, count);
+            console.log(res);
+            count = count + 1;
+            this.send();
+        } catch (error) {
+            console.log(error);
+            count = 1;
+            events.unshift(event);
+            console.log('Retrying sending ' + event);
+            this.send();
+        }
+    }
+}
+
 function SDK () {
-    if(this instanceof SDK) {
-
-    }
-    if(!SDK.instance) {
-        let events = [];
-        let count = 1;
-
-        this.logEvent = function (event) {
-            events.push(event);
+    SDK.instance;
+    const getInstance = function () {
+        debugger
+        if(!SDK.instance) {
+            SDK.instance = new Analytics();
         }
-
-        this.send = async function () {
-            const event = events.shift();
-            if(!event) {
-                console.log('all sent');
-                return;
-            }
-            try {
-                const res = await sendEvent(event, count);
-                console.log(res);
-                count = count + 1;
-                this.send();
-            } catch (error) {
-                console.log(error);
-                count = 1;
-                events.unshift(event);
-                console.log('Retrying sending ' + event);
-                this.send();
-            }
-        }
-        SDK.instance = this;
+        return SDK.instance
     }
-    return SDK.instance;
+
+    return {
+        getInstance: getInstance
+    }
 }
 
 
 
 const sdk = new SDK();
 const sdk2 = new SDK();
+console.log(sdk.getInstance() === sdk2.getInstance());
 
-console.log(sdk == sdk2);
+// export default sdk;
 
-sdk.logEvent("event 1");
-sdk.logEvent("event 2");
-sdk.logEvent("event 3");
-sdk.logEvent("event 4");
-sdk.logEvent("event 5");
-sdk.logEvent("event 6");
-sdk.logEvent("event 7");
-sdk.logEvent("event 8");
+// sdk.logEvent("event 1");
+// sdk.logEvent("event 2");
+// sdk.logEvent("event 3");
+// sdk.logEvent("event 4");
+// sdk.logEvent("event 5");
+// sdk.logEvent("event 6");
+// sdk.logEvent("event 7");
+// sdk.logEvent("event 8");
 
-sdk.send();
+// sdk.send();
 
-sdk2.logEvent("event 9");
-sdk2.logEvent("event 10");
+// sdk2.logEvent("event 9");
+// sdk2.logEvent("event 10");
