96cd4b5ee021395c8948b8789bb0e4520c5ca15f
Salah Ansari <salah.env@gmail.com>
2025-01-15 22:16:57 +0530

    refined

---
+++ analyticsSdk/index.js
@@ -0,0 +1,64 @@
+const sendEvent = (value, count) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            count === 5 ?
+            reject('Failed to sent ' + value) : resolve('Analytics sent ' + value)
+        }, 1000);
+    });
+}
+function SDK () {
+    if(this instanceof SDK) {
+
+    }
+    if(!SDK.instance) {
+        let events = [];
+        let count = 1;
+
+        this.logEvent = function (event) {
+            events.push(event);
+        }
+
+        this.send = async function () {
+            const event = events.shift();
+            if(!event) {
+                console.log('all sent');
+                return;
+            }
+            try {
+                const res = await sendEvent(event, count);
+                console.log(res);
+                count = count + 1;
+                this.send();
+            } catch (error) {
+                console.log(error);
+                count = 1;
+                events.unshift(event);
+                console.log('Retrying sending ' + event);
+                this.send();
+            }
+        }
+        SDK.instance = this;
+    }
+    return SDK.instance;
+}
+
+
+
+const sdk = new SDK();
+const sdk2 = new SDK();
+
+console.log(sdk == sdk2);
+
+sdk.logEvent("event 1");
+sdk.logEvent("event 2");
+sdk.logEvent("event 3");
+sdk.logEvent("event 4");
+sdk.logEvent("event 5");
+sdk.logEvent("event 6");
+sdk.logEvent("event 7");
+sdk.logEvent("event 8");
+
+sdk.send();
+
+sdk2.logEvent("event 9");
+sdk2.logEvent("event 10");
